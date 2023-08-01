95a884f462a36a3d46a9bdb6b2dafc7b2b3a4cc1
Zaheer Bashir <xaheerbashir@yahoo.com>
2023-08-01 11:09:55 +0500

    All Bugs is fixed

---
+++ src/App.js
@@ -0,0 +1,97 @@
+import React, { useState } from 'react'
+import './App.css';
+import Navbar from './Components/Navbar';
+import TextFrom from './Components/TextFrom';
+import About from './Components/About';
+import Alert from './Components/Alert';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Contact from './Components/Contact';
+
+function App(props) {
+
+  const [mode, setmode] = useState("light");
+  const [alert, setAlert] = useState(false);
+
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1800);
+  }
+
+  const bgchange= () => {
+    document.body.classList.add("body-light");
+  }
+  
+  bgchange();
+  
+  const toggleMode = () => {
+    if(mode === "light"){
+      setmode("dark");
+      document.body.style.background = "linear-gradient(41deg, rgba(128,128,128,1) 50%, rgba(0,0,0,1) 50%)";
+      document.body.style.color = "white";
+      document.documentElement.classList.add("dark-mode");
+      showAlert("Dark mode has been Enabled", "success");
+    } else {
+      setmode("light");
+      document.body.style.background = 'linear-gradient(41deg, rgba(241,241,241,1) 50%, rgba(158,232,255,1) 50%)';
+      document.body.style.color = "black";
+      document.documentElement.classList.remove("dark-mode");
+      showAlert("Light mode has been Enabled", "success");
+    }
+  }
+
+
+  return (
+    <React.Fragment>
+      <Router>      
+        <Navbar mode={mode} toggleMode={toggleMode} title="TextUtils" home="Home" about="About" contact="Contact Us" />
+        <Alert alert={alert}/>
+        <div className='container my-3'>
+          <Routes>
+          <Route
+              path="/home"
+              element={
+                <TextFrom
+                  showAlert={showAlert}
+                  heading="Text Converter and Analysis"
+                  mode={mode}
+                  UpperCase="Convert UpperCase"
+                  LowerCase="Convert LowerCase"
+                  Clear="Clear Text"
+                  Copy="Copy To Clipboard"
+                  ExtraSpace="Remove Extra Spaces"
+                  alert={alert}
+                />
+              }
+            />
+          <Route
+              path="/"
+              element={
+                <TextFrom
+                  showAlert={showAlert}
+                  heading="Text Converter and Analysis"
+                  mode={mode}
+                  UpperCase="Convert UpperCase"
+                  LowerCase="Convert LowerCase"
+                  Clear="Clear Text"
+                  Copy="Copy To Clipboard"
+                  ExtraSpace="Remove Extra Spaces"
+                  alert={alert}
+                />
+              }
+            />
+            <Route exact path="/about" element={<About mode={mode}/>} />
+            <Route exact path="/contact" element={<Contact mode={mode}/>} />
+          </Routes>
+        </div>
+      </Router>
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/Components/TextFrom.js
@@ -0,0 +1,85 @@
+import React, { useState } from 'react';
+
+export default function TextFrom(props) {
+    
+    const [text, setText] = useState("");
+    
+    const isbtnempty = e => {
+    }
+    
+    const handleUpperCase = () => {
+        let newText = text.toUpperCase();
+        setText(newText);
+        props.showAlert("Converted to UpperCase", "success");
+        if(newText === ""){
+            props.showAlert("Please write anything to convert in uppercase...", "danger");
+        }
+    }
+    
+    const handleOnChange = (event) => {
+        setText(event.target.value)
+    }
+    
+    const handleLowerCase = () => {
+        let newText = text.toLowerCase();
+        setText(newText);
+        props.showAlert("Converted to LowerCase", "success");
+        if(newText === ""){
+            props.showAlert("Please write anything to Convert in lowercase...", "danger");
+        }
+    }
+    
+    const handleClear = () => {
+        let newText = "";
+        setText(newText);   
+        props.showAlert("Text Cleared", "success");
+        if(newText === ""){
+            props.showAlert("Textbox is already empty..", "danger");
+        }
+    }
+    
+    const handleCopy = () => {
+        let textarea = document.getElementById("exampleFormControlTextarea1").value;
+        navigator.clipboard.writeText(textarea);
+        props.showAlert("Copied to Clipboard", "success");
+        if(textarea === ""){
+            props.showAlert("Please write anything for copying text...", "danger");
+        }
+    }
+    
+    const handleRemoveExtraSpaces = () => {
+        let newText = text.trim().replace(/\s+/g, ' ');
+        setText(newText);
+        props.showAlert("Extra Spaces Removed", "success");
+        if(newText === ""){
+            props.showAlert("Please write anything for remove spaces...", "danger");
+        }
+    }
+
+    return (
+    <React.Fragment>
+        <div className="container text-form" style={{marginTop: "50px"}}>
+            <h1 className="text-center">{props.heading}</h1>
+            <textarea className="form-control" onChange={handleOnChange} id="exampleFormControlTextarea1" rows="10" placeholder="Enter the text here" value={text}
+             style={{backgroundColor: props.mode === "light" ? "white" : "lightgray"}}
+            ></textarea>
+            <button className="btn btn-primary my-3 mx-1" onClick={handleUpperCase}>{props.UpperCase}</button>
+            <button className="btn btn-primary mx-1 my-2" onClick={handleLowerCase}>{props.LowerCase}</button>
+            <button className="btn btn-danger mx-1 my-2" onClick={handleClear}>{props.Clear}</button>
+            <button className="btn btn-success mx-1 my-2" onClick={handleCopy}>{props.Copy}</button>
+            <button className="btn btn-primary mx-1 my-2" onClick={handleRemoveExtraSpaces}>{props.ExtraSpace}</button>
+        </div>
+        <div className="container my-4">
+            <h1>Summary Of Your Text</h1>
+            <p>Number of words: {text.trim() === '' ? 0 : text.trim().split(/\s+/).length}</p>
+            <p>Number of Charactors: {text.length}</p>
+            <h3 className="text-center my-3">Preview Document</h3>
+            <textarea readOnly={true} className="form-control" onChange={handleOnChange} id="exampleFormControlTextarea1" rows="5" style={{backgroundColor: props.mode === "light" ? "white" : "lightgray", resize: "none"}} value={text
+            }
+            ></textarea>
+        </div>
+    </React.Fragment>
+  )
+}
+
+
